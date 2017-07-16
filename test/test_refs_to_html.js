ea006c9acd0b2ad25349bccbae64188ce07e5146
Samuel Marks <samuelmarks@gmail.com>
2017-07-17 02:48:52 +1000

    Changed citation style; added deterministic JSON.stringify for risk.json

--- test/test_refs_to_html.js
+++ test/test_refs_to_html.js
@@ -7,9 +7,12 @@ var risk_json = require('../risk');
 describe('test ref to HTML', function () {
     it('turns refs to HTML', function (done) {
         var Cite = require('citation-js');
-        var res_html = (new Cite(__1.get_all_refs(risk_json))).get({ format: 'string', type: 'html' });
+        var jsonStableStringify = require('json-stable-stringify');
+        var res_html = (new Cite(__1.get_all_refs(risk_json))).get({
+            format: 'string', type: 'html', style: 'citation-harvard1', lang: 'en-US'
+        });
         risk_json.html_of_all_refs = JSON.stringify(res_html);
-        fs_1.writeFile('risk.json', JSON.stringify(risk_json, null, 4), 'utf8', function (err) {
+        fs_1.writeFile('risk.json', jsonStableStringify(risk_json, { space: 4 }), 'utf8', function (err) {
             if (err)
                 return done(err);
             var er = void 0;
